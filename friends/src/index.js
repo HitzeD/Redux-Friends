1c4eb20379d2597b650627c72f49d403e5d1ddfb
HitzeD <d-hitze@comcast.net>
2019-05-16 15:47:06 -0500

    file structure started

--- friends/src/index.js
+++ friends/src/index.js
@@ -8,12 +8,13 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from './reducers';
+import Logger from 'redux-logger';
 
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(
   rootReducer,
-  composeEnhancers(applyMiddleware(thunk))
+  composeEnhancers(applyMiddleware(thunk, Logger))
 );
 
 ReactDOM.render(
---
+++ friends/src/reducers/index.js
@@ -0,0 +1,13 @@
+const initialState = {
+    friends: [],
+    isLoading: false
+};
+
+function reducer(state = initialState, action){
+    switch(action.type){
+        default:
+            return state;
+    }
+}
+
+export default reducer;
